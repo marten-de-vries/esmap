9a4cf12daef33146fcc69c9c2308f6707cdca2e9
Nathan Zadoks <nathan@nathan7.eu>
2013-12-03 23:22:48 +0100

    handle child arrays

--- README.md
+++ README.md
@@ -12,4 +12,7 @@
   Maps over the given node's children with the given function.
   Returns a fresh node.
 
+  If a node has a property with an array of children, `fn(child, key, node)` is called for every child as if they were direct descendants, but the truthy return values are `Array#concat`'d together.
+  A falsy value is ignored (the node is removed from the array), an array value is concatenated, a non-array value is placed into the array.
+
 [esprima]: http://esprima.org
--- index.js
+++ index.js
@@ -2,14 +2,23 @@
 var keys = require('./keys')
   , objmap = require('objmap')
   , __hop = {}.hasOwnProperty
-  
-module.exports = function(node, fn) {
+
+module.exports = function map(node, fn) {
+  if (!node) return node
+
   var newNode =
       objmap.keys(keys[node.type], function(key) {
-        return fn(node[key], key, node)
+        var child = node[key]
+        if (Array.isArray(child))
+          return child.reduce(function(acc, child) {
+            return acc.concat(fn(child, key, node) || [])
+          }, [])
+        return fn(child, key, node)
       })
+
   for (var key in node)
     if (__hop.call(node, key) && !__hop.call(newNode, key))
       newNode[key] = node[key]
+
   return newNode
 }
